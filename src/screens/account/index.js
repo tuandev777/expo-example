d6ccfbe9e577f84e22ea61535421079bfe1545b9
PhamThanhTuan <phamthanhtuan.14@gmail.com>
2022-07-31 12:06:36 +0700

    finger print

--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -1,16 +1,29 @@
 import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import ButtonElement from "../../components/elements/ButtonElement";
+import TextElement from "../../components/elements/TextElement";
 import SCREENS_NAME from "../../constants/screens";
-
+import * as LocalAuthentication from "expo-local-authentication";
 const AccountScreen = (props) => {
   const { navigation } = props;
+
+  const testFingerPrint = async () => {
+    try {
+      const res = await LocalAuthentication.authenticateAsync();
+      console.log("res", res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate(SCREENS_NAME.HOME_SCREEN)}
-      >
-        <Text>Account</Text>
-      </TouchableOpacity>
+      <TextElement title="Account" />
+      <ButtonElement
+        title="Navigate to Home"
+        onPress={() => navigation.navigate(SCREENS_NAME.ACCOUNT_SCREEN)}
+      />
+
+      <ButtonElement title="Finger Print" onPress={testFingerPrint} />
     </View>
   );
 };
